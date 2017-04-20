f9f653305a5e251e6b91336e78fba9946b53fd47
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-04-19 23:56:42 -0400

    Lesson 3 solutions

---
+++ lesson_3/question4.rb
@@ -0,0 +1,6 @@
+# Question 4
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers.delete_at(1) # deletes the value "2" at index 1 in the array "numbers"
+numbers.delete(1)    # deletes the number 1 from the array "numbers"
---
+++ lesson_3/question5.rb
@@ -0,0 +1,5 @@
+def ten_to_one_hundred?(number)
+  (10..100).include?(number)
+end
+
+puts ten_to_one_hundred?(42)  # Returns true
