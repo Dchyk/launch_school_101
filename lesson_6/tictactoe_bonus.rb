c4a17449016b49af64c92d55f517bbb4de32b24a
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-05-16 17:33:31 -0400

    Work in progress on tic tac toe bonus features

--- lesson_6/tictactoe_bonus.rb
+++ lesson_6/tictactoe_bonus.rb
@@ -1,10 +1,18 @@
 # Tic Tac Toe
 
-require 'pry'
+require 'pry-byebug'
 
-WINNING_LINES = [[1, 3, 3], [4, 5, 6], [7, 8, 9]] + # rows
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
+# TWO_IN_ROW =  {top: [[1, 2], [2, 3], [1, 3]], 
+#               middle: [[4, 5], [5, 6], [4, 6]],
+#               bottom: [[7, 8], [8, 9], [7, 9]]}
+
+TWO_IN_ROW = {top: [1, 2, 3], middle: [4, 5, 6], bottom: [7, 8, 9]}
+TOP_ROW = [1, 2, 3]
+MIDDLE_ROW = [4, 5, 6]
+BOTTOM_ROW = [7, 8, 9]
 
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
@@ -46,6 +54,12 @@ def display_board(brd)
 end
 # rubocop:enable Metrics/MethodLength
 
+def display_score(score_hash)
+  prompt "Score:"
+  prompt "Player: #{score_hash['Player']}. Computer: #{score_hash['Computer']}."
+end
+
+
 def initialize_board
   new_board = {}
   (1..9).each { |num| new_board[num] = INITIAL_MARKER }
@@ -68,9 +82,29 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
-  brd[square] = COMPUTER_MARKER
+# def computer_places_piece!(brd)                    # original comp places piece
+#   square = empty_squares(brd).sample
+#   brd[square] = COMPUTER_MARKER
+# end
+
+# New computer places piece:
+
+def computer_places_piece!(brd)  
+  # Computer plays defense, and plays an open square on a row where player has 2 squares
+  if brd.values_at(*TOP_ROW).count(PLAYER_MARKER) == 2 && 
+    brd.values_at(*TOP_ROW).count(COMPUTER_MARKER) == 0
+    computer_next_square = TOP_ROW.select { |square| brd[square] == INITIAL_MARKER }.first
+  elsif brd.values_at(*MIDDLE_ROW).count(PLAYER_MARKER) == 2 && 
+    brd.values_at(*MIDDLE_ROW).count(COMPUTER_MARKER) == 0
+    computer_next_square = MIDDLE_ROW.select { |square| brd[square] == INITIAL_MARKER }.first
+  elsif brd.values_at(*BOTTOM_ROW).count(PLAYER_MARKER) == 2 && 
+    brd.values_at(*BOTTOM_ROW).count(COMPUTER_MARKER) == 0
+    computer_next_square = BOTTOM_ROW.select { |square| brd[square] == INITIAL_MARKER }.first
+  else
+    computer_next_square = empty_squares(brd).sample  # If player not threatening a row, computer picks randomly
+  end
+
+  brd[computer_next_square] = COMPUTER_MARKER
 end
 
 def board_full?(brd)
@@ -102,6 +136,8 @@ def detect_winner(brd)
   nil
 end
 
+score = {'Player'=>0, 'Computer'=>0}  # Initialize scoring
+
 loop do
   board = initialize_board
   display_board(board)
@@ -120,8 +156,16 @@ loop do
 
   if someone_won?(board)
     prompt "#{detect_winner(board)} won!"
+    score[detect_winner(board)] += 1
   else
     prompt "It's a tie!"
+  end
+
+  display_score(score)
+
+  if score.any? { |_, score| score == 5}
+    prompt "#{detect_winner(board)} won 5 times! The game is over!"  # Should display whatever player just hit 5
+    break
   end
 
   prompt "Play again? (y or n)"
