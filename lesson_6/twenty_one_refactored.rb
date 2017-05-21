8d7b5364c96ae4906b3de1526493242830b8f5a5
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-05-21 11:25:26 -0400

    Twenty-one refactored + all bonus features

--- lesson_6/twenty_one_refactored.rb
+++ lesson_6/twenty_one_refactored.rb
@@ -1,4 +1,4 @@
-# Twenty-One Refactored
+# Twenty-One Refactored + bonus features!
 require 'pry'
 
 CARD_VALUES = { 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9,
@@ -42,13 +42,13 @@ def busts?(hand_total)
   hand_total > BLACKJACK
 end
 
-def return_winner(dealer, table, player) # cards
+def return_winner(dealer_hand_total, player_hand_total) # totals
   winner = ''
-  if calculate_dealer_hand(dealer, table) > calculate_hand(player)
+  if dealer_hand_total > player_hand_total
     winner = 'Dealer'
-  elsif calculate_dealer_hand(dealer, table) < calculate_hand(player)
+  elsif dealer_hand_total < player_hand_total
     winner = 'You'
-  elsif calculate_dealer_hand(dealer, table) == calculate_hand(player)
+  elsif dealer_hand_total == player_hand_total
     winner = 'No one'
   end
   winner
@@ -64,9 +64,29 @@ def display_winner(winner)
   end
 end
 
-def display_totals(dealer, table, player) # cards
-  prompt "Dealer has #{calculate_dealer_hand(dealer, table)}" \
-         " and you have #{calculate_hand(player)}"
+def display_totals(dealer, player) # cards
+  prompt "Dealer has #{dealer}" \
+         " and you have #{player}"
+  puts " "
+end
+
+def display_if_overall_winner(player_win_total, dealer_win_total)
+  if player_win_total == 5
+    puts " "
+    prompt "You won 5 times! Congrats, you are the overall winner!"
+    puts " "
+  elsif dealer_win_total == 5
+    puts " "
+    prompt "The dealer won 5 times - game over, the dealer won!"
+    puts " "
+  end
+end
+
+def reset_score_if_overall_winner(player_win_total, dealer_win_total)
+  if player_win_total == 5 || dealer_win_total == 5
+    player_win_total = 0
+    dealer_win_total = 0
+  end
 end
 
 def deal_cards(player, dealer, dealer_table_card, current_deck)
@@ -75,6 +95,9 @@ def deal_cards(player, dealer, dealer_table_card, current_deck)
   dealer_table_card << deal_card(current_deck)
 end
 
+player_wins = 0 # Running total of player wins
+dealer_wins = 0 # Running total of dealer wins
+
 loop do # Begin outer main game loop
   deck = create_deck # Create a fresh deck for the game
 
@@ -82,6 +105,8 @@ loop do # Begin outer main game loop
   player_hand       = []
   dealer_hand       = []
   table_card        = [] # The dealer's facedown table card
+  player_total      = 0  # Total value of player's hand
+  dealer_total      = 0  # Total value of dealer's hand
 
   # Begin single game loop
   loop do
@@ -95,10 +120,12 @@ loop do # Begin outer main game loop
       prompt "You have:   #{display_hand(player_hand)}"
       puts " "
 
-      # Check if player busted - only happens after at least 1 hit
-      break if busts?(calculate_hand(player_hand))
+      player_total = calculate_hand(player_hand)
 
-      prompt "Your current hand total is #{calculate_hand(player_hand)}."
+      # Check if player busted - only happens after at least 1 hit
+      break if busts?(player_total)
+
+      prompt "Your current hand total is #{player_total}."
       prompt "Do you want to hit or stay? Type 'stay' or any key to hit:"
 
       choice = gets.chomp
@@ -110,11 +137,14 @@ loop do # Begin outer main game loop
 
     if busts?(calculate_hand(player_hand))
       prompt "You busted! Dealer wins!"
+      dealer_wins += 1
       break
     else # Dealer hits up to at least 17
       while calculate_dealer_hand(dealer_hand, table_card) < DEALER_MAX
         dealer_hand << deal_card(deck)
       end
+
+      dealer_total = calculate_dealer_hand(dealer_hand, table_card)
 
       system 'clear'
       prompt "Dealer flips his table card over..."
@@ -123,22 +153,35 @@ loop do # Begin outer main game loop
       prompt "You have:   #{display_hand(player_hand)}"
       puts " "
 
-      if busts?(calculate_dealer_hand(dealer_hand, table_card))
-        display_totals(dealer_hand, table_card, player_hand)
+      if busts?(dealer_total)
+        display_totals(dealer_total, player_total)
         prompt "You win! The dealer busted!"
+        player_wins += 1
       else
-        display_totals(dealer_hand, table_card, player_hand)
-        display_winner(return_winner(dealer_hand, table_card, player_hand))
+        display_totals(dealer_total, player_total)
+        display_winner(return_winner(dealer_total, player_total))
       end
     end
 
     break
   end # End of single game loop
 
+  if return_winner(dealer_total, player_total) == 'You' && !busts?(player_total)
+    player_wins += 1
+  elsif return_winner(dealer_total, player_total) == 'Dealer' && !busts?(dealer_total)
+    dealer_wins += 1
+  end
+
   puts " "
+  prompt "Total wins - Player: #{player_wins}  Dealer: #{dealer_wins}"
+
+  display_if_overall_winner(player_wins, dealer_wins)
+  reset_score_if_overall_winner(player_wins, dealer_wins)
+
   prompt "Wanna go again? Type 'y' to play again or any key to quit."
   again = gets.chomp
   break if again != 'y'
 end # End of main outer game loop
+
 
 prompt "Thanks for playing Twenty-One! I had fun taking your money."
