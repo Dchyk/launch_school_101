b1fa3f7581fe6d572ccdf09e70382cbeb42538ad
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-05-26 19:15:51 -0400

    Easy 5 exercises

---
+++ 101_109_sm_problems/easy_5/alphabetical_numbers.rb
@@ -0,0 +1,48 @@
+# Easy 5 Exercise 8 - Alphabetical numbers
+
+# input
+# - an array of integers
+#
+# output
+# - an array of integers sorted based on the English words for each 
+#   number
+#
+# algorithm
+# - take the input array use the values to look up hash keys, each value
+#   being the english word for the number, and sort the input array according
+#   to the hash values 
+
+ENGLISH_NUMBERS = {
+  0 => 'zero',
+  1 => 'one',
+  2 => 'two',
+  3 => 'three',
+  4 => 'four',
+  5 => 'five',
+  6 => 'six',
+  7 => 'seven',
+  8 => 'eight',
+  9 => 'nine',
+  10 => 'ten',
+  11 => 'eleven',
+  12 => 'twelve',
+  13 => 'thirteen',
+  14 => 'fourteen',
+  15 => 'fifteen',
+  16 => 'sixteen',
+  17 => 'seventeen',
+  18 => 'eighteen',
+  19 => 'nineteen',
+  20 => 'twenty'
+}
+
+def alphabetic_number_sort(arr)
+  arr.sort do |a, b|
+    ENGLISH_NUMBERS[a] <=> ENGLISH_NUMBERS[b]
+  end
+end
+
+alphabetic_number_sort((0..19).to_a) == [
+  8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
+  6, 16, 10, 13, 3, 12, 2, 0
+]
