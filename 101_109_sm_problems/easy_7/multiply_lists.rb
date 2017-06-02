5603e32a807cd99f82b4d8a3dad884dc134cf443
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-06-02 09:32:15 -0400

    Easy 7

---
+++ 101_109_sm_problems/easy_7/multiply_lists.rb
@@ -0,0 +1,18 @@
+# Easy 7 Exercise 8 - Multiply Lists
+
+# input
+# - two arrays, each lists of integers
+# 
+# output
+# - a new array containing the products of each pair of numbers that have the
+#   same index in the input arrays
+# - each input array has the same number of elements
+
+def multiply_list(array1, array2)
+  array1.map.with_index { |num, index| num * array2[index] }
+end
+
+# Examples
+
+puts multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+puts multiply_list([7, 8, 9], [10, 11, 12])
